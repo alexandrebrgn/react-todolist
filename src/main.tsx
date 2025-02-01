8fef9eec86aafd6c3ade90d1c0220aa3518bb7a1
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-02-01 13:19:56 +0100

    correction : remove inexistant import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { MyToDoListProvider } from "@/context/to_do_list_context.tsx";
 
