92a1a3e914c45632b75c95ed5cdaead48cda1301
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-02-01 21:55:47 +0100

    correction : some modifications to make `pnpm run build` possible

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'node:path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
     resolve: {
         alias: {
             '@': resolve(__dirname, './src'),
         },
     },
-    plugins: [
-        react(),
-    ],
-})
+    plugins: [react()],
+    build: {
+        outDir: 'dist', // Dossier de sortie pour les fichiers de production
+        assetsDir: 'assets', // Dossier pour les assets (CSS, images, etc.)
+        emptyOutDir: true, // Vide le dossier de sortie avant de construire
+    },
+});
