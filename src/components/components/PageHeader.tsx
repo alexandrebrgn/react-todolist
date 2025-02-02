8884a8f3fa1ca68703a6ea2cdbc756dfb9a42799
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-02-02 22:44:47 +0100

    fix : fixed modeToggle that wasn't reacting

--- src/components/components/PageHeader.tsx
+++ src/components/components/PageHeader.tsx
@@ -3,8 +3,8 @@ import {CheckSquare} from "@mynaui/icons-react";
 
 const PageHeader = () => (
   <>
-    <div className="absolute top-0 right-0 p-7">
-      <ModeToggle/>
+    <div className="absolute top-0 right-0 p-7 z-10">
+      <ModeToggle />
     </div>
     <div className="absolute top-0 left-0 p-7">
       <CheckSquare/>
